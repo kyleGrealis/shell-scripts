b261061f4279be80e52cc55bebed088c0560df8c
Kyle Grealis <kylegrealis@icloud.com>
2023-04-07 23:48:40 -0400

    modified paths to copy and store tarball

--- daily-backup.sh
+++ daily-backup.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 echo "Daily backup: INITIATED!"
+echo
+echo
 # make a daily backup of the Documents directory
-tar -czvf ~/Backup/backup.tar.gz ~/Documents/
+tar -czf /Users/3nc0d3d_/Backup/backup.tar.gz /Users/3nc0d3d_/teek/
 echo "Daily backup: COMPLETE!"
-sleep 3
-echo "Next back-up in 24 hours."
+echo
+echo "Moving a copy to iCloud Documents..."
+cp /Users/3nc0d3d_/Backup/backup.tar.gz /Users/3nc0d3d_/Documents/
+echo
+echo "Next backup in 24 hours."
