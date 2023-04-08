b071b327ef68bf06221f130a98297346707e9591
Kyle Grealis <kylegrealis@icloud.com>
2023-04-08 00:00:11 -0400

    fixed output display

--- git_pull.sh
+++ git_pull.sh
@@ -5,14 +5,12 @@
 
 find ~/repos -maxdepth 1 -mindepth 1 -type d | while read dir; do
   echo "$dir"
-  echo
   cd $dir
   git status
-  echo && echo
   git pull
-  echo && echo && echo
+  echo
 done
 
-
+echo
 echo "All git repos updated"
 
