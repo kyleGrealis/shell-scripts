c98dbd59e2125eaafe6b808ae239b9bdc9ed886c
Kyle Grealis <kylegrealis@icloud.com>
2023-04-08 23:34:26 -0400

    cleaned up output for readability

--- git_pull.sh
+++ git_pull.sh
@@ -2,15 +2,15 @@
 
 # this script will iterate through the git repos and update them
 
-
+echo "------------------------------------------------------------------------------"
 find ~/repos -maxdepth 1 -mindepth 1 -type d | while read dir; do
   echo "$dir"
   cd $dir
   git status
   git pull
-  echo
+  echo "------------------------------------------------------------------------------"
 done
 
-echo
+echo "------------------------------------------------------------------------------"
 echo "All git repos updated"
 
