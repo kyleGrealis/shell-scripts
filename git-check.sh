d5ba36e89083f0b126df8303e2ffcf5d7da31208
Kyle Grealis <kylegrealis@icloud.com>
2023-04-09 23:07:32 -0400

    better version of repo check

---
+++ git-check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+#find ~/repos -maxdepth 1 -mindepth 1 -type d | while read dir; do
+for dir in ~/repos/* ; do
+#cd $dir
+	if [[ -d $dir/.git ]]
+	then
+		cd $dir
+		echo "------------------------------------------------------------------------------"
+		echo -e "Currently checking $dir\n"
+  		git status
+		git fetch
+	sleep 0.2
+	else
+		continue
+	fi
+done
+echo "=============================================================================="
+echo -e "\n\nAll git repos updated"
+
