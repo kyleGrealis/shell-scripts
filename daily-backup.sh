275ef65841cd13d175ae00c72c65d26af54ea57d
Kyle Grealis <kylegrealis@icloud.com>
2023-04-12 08:12:56 -0400

    changes

--- daily-backup.sh
+++ daily-backup.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+<<<<<<< HEAD
 # to be run as a cron job
 
 echo "Daily personal backup: INITIATED!"
 echo
+=======
+echo -e "Personal backup: INITIATED!\n"
+>>>>>>> e193792 (needs improvements)
 
 # make a daily backup of my personal directory
-tar -czf /Users/3nc0d3d_/Documents/backup.tar.gz /Users/3nc0d3d_/teek/
-echo "Daily file backup: COMPLETE!"
-echo
+tar -czf ~/Documents/backup.tar.gz ~/teek/ 2> /dev/null
+if [[ $? -eq 0 ]]
+  then
+    echo -e "Personal folder backup: ${bgreen}COMPLETE!${nc}\n"
+  else
+    echo -e "Personal folder backup: ${bred}FAILED!!!${nc}\n"
+fi
 
+sleep 2
 
-echo "Repo backup: INITIATED!"
-echo
-tar -czf /Users/3nc0d3d_/Documents/repos-bu.tar.gz /Users/3nc0d3d_/repos/
-echo "Daily repo backup: COMPLETE!"
-echo
-echo "Next backup in 24 hours."
+echo -e "Repo backup: INITIATED!\n"
+
+# make backup of git repositories
+tar -czf ~/Documents/repos-bu.tar.gz ~/repos/ 2> /dev/null
+if [[ $? -eq 0 ]]
+  then
+    echo -e "Repo backup: ${bgreen}COMPLETE!${nc}\n"
+  else
+    echo -e "Repo backup: ${bred}FAILED!!${nc}\n"
+fi
+
+sleep 2
+
+echo "Be sure to backup daily."
