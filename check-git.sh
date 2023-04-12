cd4c5f157f121e6c500285be1db629d084b1c068
Kyle Grealis <kylegrealis@icloud.com>
2023-04-12 09:34:46 -0400

    color output and logical flow updated

---
+++ check-git.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo -e "\n${byellow}INITIATING${nc}: git repo status check"
+sleep 1.5
+
+for dir in ~/repos/* ; do
+	if [[ -d $dir/.git ]]
+	then
+		cd $dir
+		# echo -e "Currently checking $dir\n"
+  	status=$(git status | grep -cE "modified|Untracked|'git add'")
+		if
+		  [[ $status -ge 1 ]]
+		    then
+	        echo "------------------------------------------------------------------------------"
+		      echo -e "${bred} $dir ${byellow}has changes! ${nc}"
+	        echo "------------------------------------------------------------------------------"
+	        sleep 0.5
+	        git status
+		else
+		  continue
+		fi
+	sleep 0.2
+	else
+		continue
+	fi
+done
+echo "=============================================================================="
+echo -e "${bgreen}COMPLETED${nc}: git repo status check\n"
+
